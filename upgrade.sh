32f6b22d9cdf761d6829d1a58211a7be7313217a
JaKooLit <ejhay.games@gmail.com>
2025-01-31 00:53:07 +0900

    color coding update

--- upgrade.sh
+++ upgrade.sh
@@ -7,7 +7,7 @@ ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
 NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
 WARN="$(tput setaf 166)[WARN]$(tput sgr0)"
 CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
-ORANGE=$(tput setaf 9)
+ORANGE=$(tput setaf 5)
 YELLOW=$(tput setaf 3)
 RESET=$(tput sgr0)
 
