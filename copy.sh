9fe383437ba32ef4afb03fb1855bf962a482caeb
JaKooLit <ejhay.games@gmail.com>
2025-03-12 22:10:19 +0900

    activated hyprcursor for Debian and ubuntu

--- copy.sh
+++ copy.sh
@@ -89,6 +89,15 @@ fi
 
 # End of Note for Ja:
 # ##########################################################################
+
+# activating hyprcursor on env by checking if the directory ~/.icons/Bibata-Modern-Ice/hyprcursors exists
+if [ -d "$HOME/.icons/Bibata-Modern-Ice/hyprcursors" ]; then
+    HYPRCURSOR_ENV_FILE="config/hypr/UserConfigs/ENVariables.conf"
+    echo "${INFO} Bibata-Hyprcursor directory detected. Activating Hyprcursor...." 2>&1 | tee -a "$LOG" || true
+    sed -i 's/^#env = HYPRCURSOR_THEME,Bibata-Modern-Ice/env = HYPRCURSOR_THEME,Bibata-Modern-Ice/' "$HYPRCURSOR_ENV_FILE"
+    sed -i 's/^#env = HYPRCURSOR_SIZE,24/env = HYPRCURSOR_SIZE,24/' "$HYPRCURSOR_ENV_FILE"
+fi
+
 printf "\n%.0s" {1..1} 
 
 # Function to detect keyboard layout using localectl or setxkbmap
