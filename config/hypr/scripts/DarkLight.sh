23daa131508355e5e3049151e472397fae5242e8
JaKooLit <ejhay.games@gmail.com>
2025-03-13 23:06:11 +0900

    edited Dark-Light.sh to not close kitty during theme switch

--- config/hypr/scripts/DarkLight.sh
+++ config/hypr/scripts/DarkLight.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
 # For Dark and Light switching
-# Note: Scripts are looking for keywords Light or Dark except for wallpapers as the are in a separate folders
+# Note: Scripts are looking for keywords Light or Dark except for wallpapers as the are in a separate directories
 
 # Paths
 wallpaper_base_path="$HOME/Pictures/wallpapers/Dynamic-Wallpapers"
@@ -21,7 +21,7 @@ pallete_dark="dark16"
 pallete_light="light16"
 
 # intial kill process
-for pid in kitty waybar rofi swaync ags swaybg; do
+for pid in waybar rofi swaync ags swaybg; do
     killall -SIGUSR1 "$pid"
 done
 
@@ -114,6 +114,10 @@ else
 	sed -i '/^background /s/^background .*/background #dddddd/' "${kitty_conf}"
 	sed -i '/^cursor /s/^cursor .*/cursor #000000/' "${kitty_conf}"
 fi
+
+for pid_kitty in $(pidof kitty); do
+    kill -SIGUSR1 "$pid_kitty"
+done
 
 # Set Dynamic Wallpaper for Dark or Light Mode
 if [ "$next_mode" = "Dark" ]; then
@@ -235,7 +239,7 @@ ${SCRIPTSDIR}/WallustSwww.sh &&
 
 sleep 2
 # kill process
-for pid1 in kitty waybar rofi swaync ags swaybg; do
+for pid1 in waybar rofi swaync ags swaybg; do
     killall "$pid1"
 done
 
